21e1d59c81fe8baf60d71d2f771b00f5597ae125
Joe David <javid92@gmail.com>
2019-08-24 01:32:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,7 @@ def map_to_negativize(source_array)
     counter = 0
     while counter < source_array.length do
       if source_array[counter] == false
-        return false
+        then return false
        end
        counter+= 1
     end
