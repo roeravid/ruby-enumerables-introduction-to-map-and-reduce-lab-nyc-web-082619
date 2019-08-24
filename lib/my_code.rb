423fdb244c900f04e810889afc9383bc147ed5d7
Joe David <javid92@gmail.com>
2019-08-24 00:29:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,13 +37,15 @@ def map_to_negativize(source_array)
   end
   squared
  end
-  #def map_to_square(source_array)
-    #source_array.each {|x| x**=2}
-  #end
 
-  #def reduce_to_total(source_array, starting_point)
-    #starting_point = 0
-  #end
+  def reduce_to_total(source_array, starting_point)
+    reduce_to_total = []
+    counter = 0
+    while counter < source_array.length do
+      reduce_to_total += source_array[counter]
+    end
+    counter =+1
+  end
 
   def reduce_to_all_true(source_array)
     counter = 0
