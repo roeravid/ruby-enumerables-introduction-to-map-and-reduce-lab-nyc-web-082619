c4e866ae0d02bfa1de2683da7dc9742d7382806e
Joe David <javid92@gmail.com>
2019-08-24 01:17:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,6 @@ def map_to_negativize(source_array)
  end
 
   def reduce_to_total(source_array, starting_point=0)
-    starting_point = 100
     counter = 0
     while counter < source_array.length do
       total += source_array[counter]
